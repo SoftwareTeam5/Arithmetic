c5e75b0e066b7858ccd438dff6ee2ee8969919ea
weeway <geverway@gmail.com>
2016-12-07 10:58:19 +0800

    增加重复算式判断

--- Arithmetic/src/com/software/calculator/Equation.java
+++ Arithmetic/src/com/software/calculator/Equation.java
@@ -1,4 +1,5 @@
 package com.software.calculator;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -6,10 +7,9 @@ import java.util.Random;
  * Created on 2016/12/3.
  */
 public class Equation {
-    private static char[] symbol = new char[4];
-    private static String[] Equation = new String[1000];
-    private static int amount = 100;
-    
+
+    private static ArrayList<String> Record = new ArrayList<>();
+
     /**
      * 随机生成四则运算符(+、-、*、/)
      * @return symbol
@@ -56,52 +56,55 @@ public class Equation {
 
     /**
      * 生成四则运算表达式
-     * 表达式保存在数组 Equation
-     * @return Nothing
+     * @return equation
      */
-    public static void createEquation(){
+    public static String createEquation(){
         Random random = new Random();
+        String interim;
+        String finalStatus;
+        int count = random.nextInt(11)%4+2;
 
-        for(int i = 0; i < amount; ++i){
-            int count = random.nextInt(11)%4+2;
+        int operand1 = random.nextInt(11)+1;
+        int operand2 = random.nextInt(11)+1;
+        char symbol = create_symbol();
+        String strOp1 = int2String(operand1);
+        String strOp2 = int2String(operand2);
+        interim = convertEqu2Str(strOp1,strOp2,symbol);
 
-            int openrand1 = random.nextInt(11)+1;
-            int openrand2 = random.nextInt(11)+1;
-            char symbol = create_symbol();
-            String strOp1 = int2String(openrand1);
-            String strOp2 = int2String(openrand2);
-            Equation[i]= convertEqu2Str(strOp1,strOp2,symbol);
-
-            if(count > 2){
-                for(; count>2; count--){
-                    symbol = create_symbol();
-                    strOp1 = Equation[i];
-                    int change = random.nextInt(11)%2;
-                    if (change == 0){
-                        strOp1 = '('+strOp1+')';
-                    }
-                    symbol = create_symbol();
-                    openrand2 = random.nextInt(11)+1;
-                    strOp2 = int2String(openrand2);
-                    change = random.nextInt(10);
-                    if(change == 0){
-                        String temp = strOp1;
-                        strOp1 = strOp2;
-                        strOp2 = temp;
-                    }
-                    Equation[i] = convertEqu2Str(strOp1,strOp2,symbol);
+        if(count > 2){
+            for(; count>2; count--){
+                strOp1 = interim;
+                int change = random.nextInt(11)%2;
+                if (change == 0){
+                    strOp1 = '('+strOp1+')';
                 }
+                symbol = create_symbol();
+                operand2 = random.nextInt(11)+1;
+                strOp2 = int2String(operand2);
+                change = random.nextInt(10)%2;
+                if(change == 0){
+                    String temp = strOp1;
+                    strOp1 = strOp2;
+                    strOp2 = temp;
+                }
+                interim = convertEqu2Str(strOp1,strOp2,symbol);
             }
         }
+
+        finalStatus = interim;
+        return finalStatus;
     }
 
-   /**
-    * 获得四则运算表达式
-    * @return equation.
-    */
+    /**
+     * 获得四则运算表达式
+     * @return equation.
+     */
     public static String getEquation(){
-        Random random = new Random();
-        int index = random.nextInt(amount);
-        return Equation[index];
+        String equation = createEquation();
+        while (Record.contains(equation)){
+            equation = createEquation();
+        }
+        Record.add(equation);
+        return equation;
     }
 }
